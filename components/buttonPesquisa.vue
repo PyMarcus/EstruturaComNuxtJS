cf113cd66a0b03709a44d8c273a83db466691408
Marcus Vinícius Ribeiro Andrade <88283829+PyMarcus@users.noreply.github.com>
2021-11-01 18:17:57 -0300

    "ADD FUNCAO JS"

--- components/buttonPesquisa.vue
+++ components/buttonPesquisa.vue
@@ -1,7 +1,42 @@
 <template>
-    <button  class="button">Click Aqui</button>
+    <div>
+    <input id="inputt" type="text" placeholder="Pesquisar">
+    <button @click="search()" class="button">Click Aqui</button>
+    </div>
 </template>
 
 
+<script>
+   // addEventListener("click", search)
+export default{
+    methods:{
+        search: function(){
+            //let variavel = document.createElement('input')
+            //variavel.id = 'inputt'
+            //variavel.style = '../assets/css/style.css'
+            let valor = document.getElementById("inputt").value
+            if (valor != null){
+               var tabela = document.getElementById("tabela")
+               var tr = tabela.getElementsByTagName("tr")
+               var td
+               for(var i = 0; i < tr.length;i++){
+                   td = tr[i]
+                   if(td){
+                       if(td.innerHTML.indexOf(valor) > -1){
+                           tr[i].style.display = ""
+                       }else{
+                           tr[i].style.display = "none"
+                       }
+                   }
+               }
+
+            }else{
+                valor = null
+            }
+        }
+    }
+}
+
+</script>
 
 
